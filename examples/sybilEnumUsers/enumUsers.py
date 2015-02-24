2ef316209a1470b989345b9285cea4549684fa56
Rick Housley <rickyhousley+bug-r@gmail.com>
2015-02-23 22:16:39 -0500

    fixed spaces, enum in progress

--- examples/sybilEnumUsers/enumUsers.py
+++ examples/sybilEnumUsers/enumUsers.py
@@ -2,9 +2,9 @@
 
 """ enumUsers.py 
 
-	Enumerates all of the users in a given area.
+    Enumerates all of the users in a given area.
 
-	IN PROGRESS - NOT YET OPERATIONAL	
+    IN PROGRESS - NOT YET OPERATIONAL   
 """
 # Set position of a given user
 # :param fbToken Facebook token
@@ -20,113 +20,118 @@ import logging
 from pymongo import MongoClient
 
 def main(args):
-	
-	# Instantiate Logger	
-	logging.basicConfig(filename='log.log', level=logging.INFO)
-	logging.info('--------------------Logger Initiated--------------------')
+    
+    # Instantiate Logger    
+    logging.basicConfig(filename='log.log', level=logging.INFO)
+    logging.info('--------------------Logger Initiated--------------------')
 
-	# Load facebook tokens from a file
-	fbtokens = None
-	try:
-		with open(args.fbTokenFile) as f:
-			fbtokens = f.read().splitlines()
-	except IOError:
-		logging.warning('Unable to locate file provided')
-		return
-	if fbtokens is None:
-		logging.warning('Token file void of tokens')		
-		return
-	
-	logging.info('%d Facebook tokens loaded from file',len(fbtokens))
-
-
-	# Connect to database
-	client = MongoClient('localhost', 27017)
-	db = client.db
-	db_users = db.userbase
-	
-	# Create a list of Sybils
-	sybils = []
-	for token in fbtokens:	
-		try:		
-			sybils.append(happn.User(token))			
-		except NameError:
-			logging.warning('Error creating sybil, moving on to next token (most likely invalid token)')
-
-	if sybils == []:
-		logging.warning('No Sybils to work with, exiting')
-		return
-
-	logging.info('Completed sybil generation')
-
-	#Each sybil will be seperated by 500m (default radius) and proceed longitudinaly	
-	lConvFactor_km 	= 110 		# Conversion fator to degrees lat/lon in km
-	lConvFactor_m 	= 110000 	# Conversion fator to degrees lat/lon in m
-	r_m = 500
-	r_l = r_m / lConvFactor_m 	# Conversion to degrees lat/lon
-	
-	targetLat = args.width * lConvFactor_km
-	targetLon = args.height * lConvFactor_km
-
-	x=[args.lat]
-	y=args.lon	
-	for __ in range(1,len(sybils)):
-		x.append(x[-1]+r_l)		
-
-	while max(x) < targetLat:
-		while y < targetLon:			
-			idx = 0
-			for user in sybils:
-				print x[idx], y	
-				print user
-				print user.id
-				# Add a timeout for a sybil?
-				while True:
-					try:
-						user.update_activity()
-						user.set_device()	
-						time.sleep(10)				
-						user.set_position(x[idx],y)
-					except happn.HTTP_MethodError as e:	
-						time.sleep(60)
-						continue
-					break
-
-				idx+=1
-				print 'finished setting pos of sybil {}'.format(idx)
-
-				# Get Recs and save to database
-				recs = user.get_recommendations(limit=1000)				
-				logging.info('Got %d recs', len(recs))
-				
-				# Load database with new recs
-				for doc in recs:
-					db_users.insert(doc)
-
-					y=y+r_l;
-
-	x = map(lambda z:z+r_l, x)	# Add to r_l to all items in list
+    # Load facebook tokens from a file
+    fbtokens = None
+    try:
+        with open(args.fbTokenFile) as f:
+            fbtokens = f.read().splitlines()
+    except IOError:
+        logging.warning('Unable to locate file provided')
+        return
+    if fbtokens is None:
+        logging.warning('Token file void of tokens')        
+        return
+    
+    logging.info('%d Facebook tokens loaded from file',len(fbtokens))
 
 
-if __name__ == '__main__':			
-	# Generate argparse menu
-	parser = argparse.ArgumentParser()
-	
-	parser.add_argument('--fbTokenFile', required=True,
-		dest='fbTokenFile', default=None,
-		help='File loaded with Facebook tokens')
+    # Connect to database
+    client = MongoClient('localhost', 27017)
+    db = client.db
+    db_users = db.userbase
+    
+    # Create a list of Sybils
+    sybils = []
+    for token in fbtokens:  
+        try:        
+            sybils.append(happn.User(token))            
+        except NameError:
+            logging.warning('Error creating sybil, moving on to next token (most likely invalid token)')
 
-	parser.add_argument('--rLat', type=float, required=True,
-		dest='lat', help='Right corner latitude (starting point)')
+    if sybils == []:
+        logging.warning('No Sybils to work with, exiting')
+        return
 
-	parser.add_argument('--rLon', type=float, required=True,
-		dest='lon', help='Right corner longitude (starting point)')
+    logging.info('Completed sybil generation')
 
-	parser.add_argument('--width', type=float, required=True,
-		dest='width', help='Width to traverse in kilometers')
+    #Each sybil will be seperated by 500m (default radius) and proceed longitudinaly    
+    lConvFactor_km  = 110.0       # Conversion fator to degrees lat/lon in km
+    lConvFactor_m   = 110000.0    # Conversion fator to degrees lat/lon in m
+    r_m = 500.0
+    r_l = r_m / lConvFactor_m   # Conversion to degrees lat/lon
+    
+    targetLat = args.width * lConvFactor_km
+    targetLon = args.height * lConvFactor_km
 
-	parser.add_argument('--height', type=float, required=True,
-		dest='height', help='height to traverse in kilometers')
+    x=[args.lat]
+    y=args.lon  
+    for __ in range(1,len(sybils)):
+        x.append(x[-1]+r_l)     
 
-	args = parser.parse_args()	
-	main(args)
+    while max(x) < targetLat:
+        while y < targetLon:            
+            idx = 0
+            print x[idx], y 
+            for user in sybils:
+                # Add a timeout for a sybil?
+                set_pos_repeat(user, x[idx], y)
+
+                idx+=1
+                print 'finished setting pos of sybil {}'.format(idx)
+
+                # Get Recs and save to database
+                recs = user.get_recommendations(limit=1000)             
+                logging.info('Got %d recs', len(recs))
+                
+                # Load database with new recs
+                for doc in recs:
+                    db_users.insert(doc)
+
+            y=y+r_l;
+        x = map(lambda z:z+r_l, x)  # Add to r_l to all items in list
+
+
+def set_pos_repeat(user, lat,lon):
+    count = 0
+    while True:
+        # Set user position
+        print "{} Attempts".format(count)
+            
+        try:
+            user.set_device()
+            time.sleep(10)
+            user.set_position(lat,lon)
+        except happn.HTTP_MethodError as e: 
+            time.sleep(300)
+            count = count + 1
+            continue
+        break
+
+
+if __name__ == '__main__':          
+    # Generate argparse menu
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--fbTokenFile', required=True,
+        dest='fbTokenFile', default=None,
+        help='File loaded with Facebook tokens')
+
+    parser.add_argument('--rLat', type=float, required=True,
+        dest='lat', help='Right corner latitude (starting point)')
+
+    parser.add_argument('--rLon', type=float, required=True,
+        dest='lon', help='Right corner longitude (starting point)')
+
+    parser.add_argument('--width', type=float, required=True,
+        dest='width', help='Width to traverse in kilometers')
+
+    parser.add_argument('--height', type=float, required=True,
+        dest='height', help='height to traverse in kilometers')
+
+    args = parser.parse_args()  
+    main(args)
--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def readme():
         return f.read()
 
 setup(name='happn',
-		version='0.1',
-		description='Package for interacting with the Happn API',
-		url='http://github.com/rickhousley/happn',
-		author='Rick Housley',
-		author_email='RickyHousley+github@gmail.com',
-		license='MIT',
-		packages=['happn'],
-		#scripts=['bin/'],
-		install_requires = ['requests', 'logging', 'argparse'], #, 'json'], #urllib2 alread pre-packaged
-		zip_safe=False
-		)
+        version='0.1',
+        description='Package for interacting with the Happn API',
+        url='http://github.com/rickhousley/happn',
+        author='Rick Housley',
+        author_email='RickyHousley+github@gmail.com',
+        license='MIT',
+        packages=['happn'],
+        #scripts=['bin/'],
+        install_requires = ['requests', 'logging', 'argparse'], #, 'json'], #urllib2 alread pre-packaged
+        zip_safe=False
+        )
